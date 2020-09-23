c4a26beabe2887932a97aa5c93ef7bbdf2635604
Forerunner Simple Git <docker@tech.camp>
2020-09-23 13:12:35 -0400

    A file that explains the primitive commands in python

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-# This file uses an installed package to print hello world in camelcase
-import camelcase
+# String
 
-c = camelcase.CamelCase()
+my_string = "this is a string"
+print(my_string) # we can display them
 
-txt = "hello world"
-
-print(c.hump(txt))
+# Formated Strings are easy ways to include variables in string
+formated_string = f"let's display our variable in paranthesis ({ my_string }). Pay attention to the spacing."
